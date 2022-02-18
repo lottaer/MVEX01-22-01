2c09015934088ba2444497a20a20224decd95b66
le <86870982+lottaer@users.noreply.github.com>
2022-02-18 13:05:09 +0100

    Update multi_gw.cpp

--- multi_gw.cpp
+++ multi_gw.cpp
@@ -52,8 +52,7 @@ NumericVector age_prop(double p, int n, int k, double q, int hours, int st_type)
 }
 
 // This is the old simulation - does not remember age
-
-void Z_def(int i, double p, int n, int k, double q, NumericVector Z) {
+void Z_def(int i, double p, int n, int k, double q, NumericMatrix::Row Z) {
   double U = R::runif(0,1);
   int def_m = i + n;
   if(U < q) {
@@ -69,66 +68,33 @@ void Z_def(int i, double p, int n, int k, double q, NumericVector Z) {
   }
 }
 
-NumericVector Z_t(double p, int n, int k, double q, NumericVector curr, NumericVector nxt) {
+// TODO: skriv ihop denna med Z_mat
+void Z_t(double p, int n, int k, double q, NumericMatrix::Row curr, NumericMatrix::Row nxt) {
   for(int i = 0; i <= k; i++) {
-    for(int j = 0; j < curr[i]; j++ ) {
+    for(int j = 0; j < curr[i]; j++) {
       Z_def(i, p, n, k, q, nxt);
     }
   }
-  return(nxt);
 }
 
+// runs the whole simulation
 //[[Rcpp::export]]
 NumericMatrix Z_mat(double p, int n, int k, double q, int hours, NumericVector start) {
-  // gör nåt 
   NumericMatrix Z_m (hours+1, k+1);
   Z_m(0, _) = start;
   for(int i = 1; i <= hours; i++) {
-    Z_m(i,_) = Z_t(p, n, k, q, Z_m(i-1,_), Z_m(i,_));
+    Z_t(p, n, k, q, Z_m(i-1,_), Z_m(i,_));
   }
   return(Z_m);
 }
-
-// This function constructs the rows of the matrix
-NumericVector M_row_cpp(int i, double p, int n, int k, double q, NumericVector Z_r) {
-  Z_def(i, p, n, k ,q, Z_r);
-  return(Z_r);
-}
-
 
 // This functions builds the M matrix from independent rows
 //[[Rcpp::export]]
 NumericMatrix M_est_cpp(double p, int n, int k, double q) {
   NumericMatrix M (k+1, k+1);
   for(int i = 0; i <= k; i++) {
-    M(i,_) = M_row_cpp(i, p, n, k, q, M(i, _));
+    Z_def(i, p, n, k, q, M(i,_));
   }
   return(M);
 }
 
-/**
-// [[Rcpp::export]]
-NumericVector a_row(int i, double p, int n, int k) {
-  NumericVector row (k);
-  for (int j = 0; j <= k; j++)
-      row[j] = R::dbinom(i+n-j, n+i, p,0) + R::dbinom(j, n+i, p,0); 
-  return row;
-}
-// [[Rcpp::export]]
-NumericVector b_row(int i, int n, int k, double q) {
-  NumericVector b (k);
-  if(i+n <= k) {
-    b[i+n] = 1-q;
-  }
-  return b;
-}
-
-//[[Rcpp::export]]
-NumericMatrix mat(double p, int n, int k, double q, int gen) {
-  NumericMatrix M (k+1);
-  for(int i = 0; i <= k; i++){
-    M(i,_) = q*a_row(i, p, n, k) + b_row(i, n, k, q);
-  }
-  return(M);
-}
- **/
