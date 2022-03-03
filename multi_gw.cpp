6c9abe61a300e96bdb421fea490d5709891f8cca
le <86870982+lottaer@users.noreply.github.com>
2022-03-03 11:02:22 +0100

    commented

--- multi_gw.cpp
+++ multi_gw.cpp
@@ -2,94 +2,105 @@
 #include <list>
 #include <iostream>
 #include <iterator>
-
+#include <unordered_map>
 using namespace Rcpp;
 
-// replicative age?
+// defines a cell, save type and age for every object
 struct cell {
-  int type;
-  int gen;
+  int type; 
+  int age; // replicative age
 };
 
-// New simulation - we remember the ages of cells
-
+// TODO
+// Follows a mothercell until it dies
 //[[Rcpp::export]]
-NumericVector age_prop(double p, int n, int k, double q, int hours, int st_type){
-  // some starting population
-  NumericVector ages (hours+1);
-  
-  std::list<cell> Z;
-  Z.push_front({ st_type, 0 });
-  
-  for(int j = 1; j <= hours; j++) {
-    for(auto it = Z.begin(); it != Z.end(); ) {
-      double U = R::runif(0,1);
-      int def_m = it->type + n;
-      // the cell divides
-      if(U < q) {
-        int def = R::rbinom(def_m, p);
-        def_m -= def;
-        if(def <= k) {
-          Z.push_front({ def, j });
-        }
-        // The cell dies immediately
-        else {
-          ages[0]++;
-        }
-      }
-      if(def_m <= k){
-        it->type = def_m;
-        ++it;
-      }
-      else {
-        ages[j-(it->gen)]++;
-        it = Z.erase(it);
-      }
-    }
-  }
-  
-  return ages;
-}
-
-// This is the old simulation - does not remember age
-void Z_def(int i, double p, int n, int k, double q, NumericMatrix::Row Z) {
-  double U = R::runif(0,1);
-  int def_m = i + n;
-  if(U < q) {
+int cell_age(int i, double p, int n, int k){
+  cell mth = {i, 0};
+  //std::list<int> bio_age;
+  //bio_age.push_back(i);
+  // change to avoid inf loop?
+  while(mth.type <= k) {
+    int def_m = mth.type+n;
     int def = R::rbinom(def_m, p);
     def_m -= def;
-    // daughter cell
-    if(def <= k){
-      Z[def]++;
+    if(def_m <= k) {
+      mth.type = def_m;
+      mth.age++;
+      //bio_age.push_back(def_m);
+    }
+    else{
+      return mth.age;
+      //return bio_age;
     }
   }
+}
+
+/*
+NumericVector  bio_age(int i, double p, int n, int k, int trials) {
+  std::unordered_map<int, int> ages; // <time, type>
+  for(int i = 1; i <= trials; i++) {
+    cell_age(i, p, n, k, ages)
+  }
+}
+*/
+
+// Returns numeric vector with all age counts
+//[[Rcpp::export]]
+NumericVector cell_ages(int i, double p, int n, int k, int trials) {
+  int max_age = 0;
+  std::unordered_map<int, int> ages; // age and count
+  for(int ii = 1; ii <= trials; ii++) {
+    int age = cell_age(i, p, n, k);
+    // update longest lifelength observed
+    max_age = (age > max_age) ? age : max_age; 
+    ages[age]++;
+  }
+  NumericVector age_vec (max_age+1);
+  for(auto const &it: ages) {
+    age_vec[it.first] = it.second;
+  }
+  return(age_vec);
+}
+
+
+// This function is applied to all the cells
+// Let the cell divide and look at which types it becomes and verify that they survive
+void Z_def(int i, double p, int n, int k, NumericMatrix::Row Z){
+  // do something
+  int def_m = i + n;
+  int def = R::rbinom(def_m, p);
+  def_m -= def;
+  // daughter cell
+  if(def <= k){
+    Z[def]++;
+  }
+  // mother cell
   if(def_m <= k) {
     Z[def_m]++;
   }
 }
 
-// runs the whole simulation
+// Save all timesteps into a matrix
 //[[Rcpp::export]]
-NumericMatrix Z_mat(double p, int n, int k, double q, int hours, NumericVector start) {
+NumericMatrix Z_mat(double p, int n, int k, int hours, NumericVector start) {
   NumericMatrix Z_m (hours+1, k+1);
   Z_m(0, _) = start;
   for(int gen = 1; gen <= hours; gen++) {
     for(int i = 0; i <= k; i++) {
       for(int j = 0; j < Z_m(gen-1, i); j++) {
-        Z_def(i, p, n, k, q, Z_m(gen,_));
+        Z_def(i, p, n, k, Z_m(gen,_));
       }
     }
   }
   return(Z_m);
 }
 
-// This functions builds the M matrix from independent rows
+// Simulation of reproduction matrix
 //[[Rcpp::export]]
-NumericMatrix M_est_cpp(double p, int n, int k, double q) {
+NumericMatrix M_est(double p, int n, int k) {
   NumericMatrix M (k+1, k+1);
   for(int i = 0; i <= k; i++) {
-    Z_def(i, p, n, k, q, M(i,_));
+    Z_def(i, p, n, k, M(i,_));
   }
   return(M);
 }
-
